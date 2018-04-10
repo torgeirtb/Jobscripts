068e42dab002b53939ba894f6e906cf9d025692c
Torgeir Blæsterdalen <torgeirtb@gmail.com>
2018-04-10 16:25:19 +0200

    Jobscript for plotting WRF output
    Jobscript for making plots of WRF output variables in MATLAB

---
+++ JobscriptPlotting_matlab.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#:---------------------------------------------------------------
+# Jobscript for extracting data from WRF-simulations.
+#
+# Last edited: 07.April.2018, Torgeir
+#:--------------------------------------------------------------- 
+
+#SBATCH --job-name=MATLAB_plotting
+
+# Stallo account to charge
+#SBATCH -A nn9426k 
+
+# Computation resources; nodes and cores
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=50GB 
+
+# Runtime: d-hh:mm:ss  (set a bit higher than expected)
+#SBATCH --time 0-00:20:00
+
+# Give priority to this job (requires shorter jobs than 4h, and is used for testing scripts)
+# SBATCH --qos=devel
+
+# Load relevant modules
+module load netCDF/4.4.1.1-intel-2018a-HDF5-1.8.19
+module load MATLAB/R2016a-intel-2016a
+
+# Manouver to the directory containing the postprocessing script
+cd ../PostProScripts/
+
+matlab -nodisplay -nodesktop -r "WindField"
+
+exit 0
